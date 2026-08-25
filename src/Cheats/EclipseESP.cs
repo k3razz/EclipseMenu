0efd70b25e412d1ede10dff2f67ca3bc1694d7d6
kerazz <nukedlcclient@gmail.com>
2026-08-25 13:19:24 +0300

    deactivating zoom out while MatchInfoGuide active

--- src/Cheats/EclipseESP.cs
+++ src/Cheats/EclipseESP.cs
@@ -54,13 +54,8 @@ public static class EclipseESP
     {
         if (CheatToggles.zoomOut)
         {
-            if (hudManager.Chat.IsOpenOrOpening ||
-                PlayerCustomizationMenu.Instance ||
-                (Utils.isLobby &&
-                (FriendsListUI.Instance.IsOpen ||
-                 GameStartManager.Instance.LobbyInfoPane.LobbyViewSettingsPane.gameObject.active ||
-                 GameStartManager.Instance.RulesEditPanel)))
-                return;
+            if (hudManager.Chat.IsOpenOrOpening || MatchInfoGuide.Instance.IsActive || PlayerCustomizationMenu.Instance ||
+            (Utils.isLobby && (FriendsListUI.Instance.IsOpen || GameStartManager.Instance.LobbyInfoPane.LobbyViewSettingsPane.gameObject.active || GameStartManager.Instance.RulesEditPanel))) return;
 
             _resolutionChangeNeeded = true;
 
